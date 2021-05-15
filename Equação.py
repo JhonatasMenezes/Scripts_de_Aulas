3834e319b911009948b43a488ded63250670fbcf
Jhonatas Menezes <83800133+JhonatasMenezes@users.noreply.github.com>
2021-05-15 11:51:29 -0300

    Add files via upload

---
+++ Equação.py
@@ -0,0 +1,9 @@
+print('a² / b³ = x')
+n1 = int(input('Defina o valor de a: '))
+n2 = int(input('Defina o valor de b: '))
+#váriaveis definidas
+print('a² = {}'.format(n1**2))
+print('b³ = {}'.format(n2**3))
+print('{} / {} = x'.format((n1**2),(n2**3)))
+print('Com base nas informações o valor de x é {:.4}'.format((n1**2) / (n2**3)))
+
