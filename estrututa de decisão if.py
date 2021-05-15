ab84664928a3bace4342ca6ee89fbe238c7e616e
Jhonatas Menezes <83800133+JhonatasMenezes@users.noreply.github.com>
2021-05-15 14:43:51 -0300

    Add files via upload

---
+++ estrututa de decisão if.py
@@ -0,0 +1,12 @@
+estacao = input('Digite a estação do ano: ')
+
+if estacao == 'verão':
+    print('Cuidado com o Sol!')
+elif estacao == 'primavera':
+        print('Aprecie seu jardim!')
+elif estacao == 'inverno':
+    print('Agasalhe-se bem!')
+elif estacao == 'outono':
+    print('Passeie no bosque!')
+else:
+    print('Estação inválida. Verifique o que digitou!')
