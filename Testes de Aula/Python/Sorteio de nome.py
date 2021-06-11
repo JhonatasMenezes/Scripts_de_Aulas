5f90557a4ccb9129b6bea80476e641119abc0306
Jhonatas Menezes <jhoniedw.com@gmail.com>
2021-06-10 23:08:13 -0300

    Nada dms

--- Testes de Aula/Python/Sorteio de nome.py
+++ Testes de Aula/Python/Sorteio de nome.py
@@ -1,11 +1,11 @@
-#Um professor sorteando um aluno para ecrever na losa
 from time import sleep
-from random import choice 
+from random import choice
+
 n1 = str(input('Primeiro aluno: '))
-n2 = str(input('Segundo aluno: ')) 
+n2 = str(input('Segundo aluno: '))
 n3 = str(input('Terceiro aluno: '))
 n4 = str(input('Quarto aluno: '))
-lista = [n1, n2, n3, n4]
+lista = (n1, n2, n3, n4)
 escolhido = choice(lista)
 print('...')
 sleep(0.5)
