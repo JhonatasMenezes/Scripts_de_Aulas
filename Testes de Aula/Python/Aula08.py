7082d63521eb2f5b6608dffe20fb100380b6f11b
Jhonatas Menezes <jhoniedw.com@gmail.com>
2021-06-20 14:58:13 -0300

    Cirei um minigame de adivinhação.

---
+++ Testes de Aula/Python/Aula08.py
@@ -0,0 +1,6 @@
+#Utilizando módulos
+import random
+import math
+num = int(random.randint(1, 30))
+raiz = math.sqrt(num)
+print('Araiz de {} é igual a {:.3f}'.format(num, raiz))
---
+++ Testes de Aula/Python/Jogo dos Números.py
@@ -0,0 +1,27 @@
+#minigame pra adivinhar numeros de 1 à 10
+from time import sleep
+from random import randint
+
+
+cont = 'S'
+resp = 'S'
+print('=' * 25)
+print('  **JOGO DOS NÚMEROS**')
+print('Estou pensando em um número...')
+print('   Está entre 1 e 10...')
+print('   Consegue adivinhar?')
+print('=' * 25)
+while cont == resp:
+    sleep(2)
+    numPc = randint(1, 10)
+    print('PENSANDO...')
+    sleep(2)
+    palp = int(input('PRONTO... dê o seu palpite: '))
+    if palp == numPc:
+        print('PARABÉS, VOCE ACERTOOOOOOU!!!!!')
+        print('`*&`*&`*&`**`*&`*&``&`*&`*&`*&`**&`*&')
+    else:
+        print('O número certo era ', numPc)
+        print('Não foi dessa vez... :/')
+    print('=' * 25)
+    resp = input('Quer continuar? S/N: ').upper()
