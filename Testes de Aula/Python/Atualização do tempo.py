fc08f24e0db5fa0e3a684fb9d673c1f5799cd8d6
Jhonatas Menezes <jhoniedw.com@gmail.com>
2021-07-15 17:36:21 -0300

    New program

---
+++ Testes de Aula/Python/Atualização do tempo.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+import requests as rq
+from bs4 import BeautifulSoup
+
+search = 'Weather in São Bernardo do Campo'
+url = f'https://www.google.com/search?&q={search}'
+
+r = rq.get(url)
+s = BeautifulSoup(r.text,'html.parser')
+update = s.find('div',class_='BNeawe').text
+
+class MinhaGUI:
+    def __init__(self):
+        self.main_window = tk.Tk()
+        self.main_window.minsize(60,80)
+        self.main_window.title('Wheater Today!')
+        self.label1 = tk.Label(self.main_window,text= '   Olá Jhonatas, essa é previsão do tempo hoje!   ')
+        self.label2 = tk.Label(self.main_window,text= update)
+        self.label1.pack()
+        self.label2.pack()
+        tk.mainloop()
+        
+minha_gui = MinhaGUI()
