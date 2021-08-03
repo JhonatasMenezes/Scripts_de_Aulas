4648b0fb0f327b5e21965e8e9be63503e405ab9a
Jhonatas Menezes <jhoniedw.com@gmail.com>
2021-08-03 17:49:25 -0300

    new lessons

---
+++ Python/Cores terminal.py
@@ -0,0 +1,11 @@
+#Style = 0(none), 1(Bold), 4(underline), 7(negative)
+#Text(cores) = 30(white), 31(red), 32(green), 33(yellow), 34(blue), 35(purple), 36(water blue), 37(default)
+#Background = 40(white, 41(red), 42(green), 43(yellow), 44(blue), 45(purple), 46(water blue), 47(default)
+
+#Examplos
+\033[0;30;41m
+\033[4;33;44m
+\033[1;35;43m
+\033[30;42m
+\033[m
+\033[7;30m
--- Python/Validar Triângulo.py
+++ Python/Validar Triângulo.py
@@ -1,8 +1,13 @@
-a = int(input('Primeiro lado: '))
-b = int(input('Segundo lado: '))
-c = int(input('Terceiro lado: '))
+print('\033[33m-=-\033[m'*10)
+print('\033[1;36mValidação de Triângulos\033[m')
+print('\033[33m-=-\033[m'*10)
+print('Digite os valores dos lados do triângulo.')
+
+a = int(input('\033[4;34mPrimeiro lado:\033[m '))
+b = int(input('\033[4;35mSegundo lado:\033[m '))
+c = int(input('\033[4;36mTerceiro lado:\033[m '))
 
 if (b - c) < a < (b + c) and (a - c)  < b < (a + c) and (a - b)  < c < (a + b):
-    print('Triângulo possível.')
+    print('\033[1;32mTriângulo possível.\033[m')
 else:
-    print('Triângulo impossível.')
+    print('\033[1;31mTriângulo impossível.\033[m')
