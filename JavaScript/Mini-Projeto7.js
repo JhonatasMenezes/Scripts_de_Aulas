461c85a5c8d3919d98d988007a2eebafde395ad3
Jhonatas Menezes <jhoniedw.com@gmail.com>
2021-08-16 21:34:11 -0300

    .

--- JavaScript/Mini-Projeto7.js
+++ JavaScript/Mini-Projeto7.js
@@ -1,16 +1,15 @@
 // Função de soma que retorna 
 // soma de multiplos de 3 e 5
+somar(12);
+
 function somar(limite){
     let soma = 0;
     for(let i = 1;i <= limite;i++){
-        
         if((i % 3) === 0 || (i % 5) === 0){
             soma += i;
             console.log(i,'é multiplo de 3 ou 5!')
-        } else{
+        } else
             console.log(i,'não é multiplo de 3 ou 5!')
-        }
     }
     console.log('Soma total', soma);
 }
-somar(12);
