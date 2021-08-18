d0d3ff7fdff33743720aded95660d4a6c6f1035d
Jhonatas Menezes <jhoniedw.com@gmail.com>
2021-08-17 23:05:48 -0300

    Commit final de 17/08

--- JavaScript/Arrays.js
+++ JavaScript/Arrays.js
@@ -1,20 +1,72 @@
 // Introdução a Arrays
+// constantes
+const num = [1,2,3];
+const marcas = [
+    {id:1,nome:'a'},
+    {id:2,nome:'b'}
+];
+const myrray = ['Jhonatas', 24, '12/02/1997'];
+const numeros = [1,2,3,4,5,6];
+
+
 // Add elementos
-const numeros = [1,2,3];
+console.log(num);
 //inicio
-numeros.unshift(0);
-console.log(numeros);
+num.unshift(0);
+console.log(num);
 // meio
-numeros.splice(1,0,'a');
-console.log(numeros);
+num.splice(1,0,'a');
+console.log(num);
 //final
-numeros.push(5);
-console.log(numeros);
+num.push(5);
+console.log(num);
+// exercise
+console.log(myrray);
+myrray.unshift('id: 1')
+myrray.splice(2,0,'Solteiro')
+myrray.push('email@email.com')
+console.log(myrray);
+let len = myrray.length
+console.log(len);
+
 
 // Encontrar elementos
+console.log(myrray);
+// tipos primitivos
+console.log(myrray.indexOf(24));
+console.log(myrray.lastIndexOf(24));
+console.log(myrray.indexOf(2) !== -1);
+console.log(myrray.includes('id: 1'));
+// tipo referência
+let marca = marcas.find(function(marca){
+   return marca.nome === 'a'; 
+});
+let marca2 = marcas.find(marca => marca.nome === 'a');
+console.log(marca);
+console.log(marca2);
 
 
 // Remover elementos
+console.log(numeros);
+//inicio
+let remPrm = numeros.shift()
+//meio
+let remMei = numeros.splice(2,1)
+//final
+let remUlt = numeros.pop();
+
+console.log(remPrm);
+console.log(remUlt);
+console.log(remMei);
+console.log(numeros);
+
+
+
 // Dividir Elementos
+
+
 // Dividir Arrays
+
+
 // Combinar Arrays
+
